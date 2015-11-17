f261db55e45a16223791e590c3a53367323367bd
Katie <katracey@gmail.com>
2015-11-17 14:22:15 -0500

    1

---
+++ 2015910_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015910_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    
+            
+
+           int answer = Convert.ToInt32(Console.ReadLine());
+            if (answer % 2 == 0) 
+            {
+                Console.WriteLine("even");
+                    }
+            else
+            {
+                Console.WriteLine("odd");
+                    }
+
+          
+        }
+    }
+}
